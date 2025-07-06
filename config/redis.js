bf0c2d13c016f72fc2826d33641827d9a1c01aad
PhuongPi3 <duogleuyenphuong@gmail.com>
2025-07-07 03:15:29 +0700

    Fix Redis config

--- config/redis.js
+++ config/redis.js
@@ -1,9 +1,12 @@
+// config/redis.js
 const Redis = require('ioredis');
 
 let redisClient;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.REDIS_URL) {
   redisClient = new Redis(process.env.REDIS_URL);
+  redisClient.on('connect', () => console.log('✅ Redis Connected!'));
+  redisClient.on('error', (err) => console.error('❌ Redis error:', err));
 } else {
   console.log('🔸 Redis mock: Local mode.');
   redisClient = {
