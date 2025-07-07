847ad4e7a50d1fda806f3336d9bc0c684248366e
PhuongPi3 <duogleuyenphuong@gmail.com>
2025-07-08 02:56:48 +0700

    fix bug

--- models/Comment.js
+++ models/Comment.js
@@ -1,11 +1,13 @@
+const mongoose = require('mongoose');
+
 const commentSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   mangaId: { type: mongoose.Schema.Types.ObjectId, ref: 'Manga' },
   chapterId: { type: mongoose.Schema.Types.ObjectId, ref: 'Chapter' },
   content: String,
   parentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Comment', default: null },
-  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // 👈 thêm
-  dislikes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }] // 👈 thêm
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], 
+  dislikes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }] 
 }, { timestamps: true });
 
 module.exports = mongoose.model('Comment', commentSchema);
--- routes/manga.routes.js
+++ routes/manga.routes.js
@@ -1,12 +1,17 @@
-
 const router = require('express').Router();
-const { getMangaList, getMangaDetail, searchManga, filterManga ,getMangaByGenre} = require('../controllers/manga.controller');
+const { getMangaList, getMangaDetail, searchManga, filterManga ,getMangaByGenre,recommendByHistory} = require('../controllers/manga.controller');
+
+
+// 💡 IMPORT middleware
+const { verifyToken } = require('../middleware/auth.middleware.js');
+
 
 router.get('/', getMangaList);
 router.get('/:id', getMangaDetail);
 router.get('/search', searchManga);
 router.get('/filter', filterManga);
 router.get('/genre/:genreId', getMangaByGenre); 
-router.get('/recommend/history', verifyToken, mangaController.recommendByHistory);
+router.get('/recommend/history', verifyToken, recommendByHistory);
+
 
 module.exports = router;
